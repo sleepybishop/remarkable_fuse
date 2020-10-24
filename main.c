c829a853a7cc473fba32a23ea306cc93c97cda5e
joseph calderon <calderon.joe@gmail.com>
2020-10-24 15:01:39 -0700

    performance improvement for large number of folders

--- main.c
+++ main.c
@@ -19,6 +19,7 @@
 typedef struct uuid_map_node {
   remfs_file *file;
   char *path;
+  cJSON *members;
   FILE *sh;
   int flags;
   RB_ENTRY(uuid_map_node) fwdp;
@@ -46,7 +47,7 @@ typedef struct {
   remfs_file_vec fv;
   uuid_fwd_map *fwd_map;
   uuid_rev_map *rev_map;
-  cJSON *tree;
+  cJSON *members;
 } remfs_ctx;
 
 #define IS_SVG (1 << 0)
@@ -152,11 +153,11 @@ static int remfs_getattr(const char *path, struct stat *stbuf) {
 static cJSON *get_dir_entries(remfs_ctx *ctx, const char *path) {
   cJSON *n = NULL;
   if (strcmp(path, "/") == 0) {
-    n = cJSON_GetObjectItem(ctx->tree, "root");
+    n = ctx->members;
   } else {
     uuid_map_node *ref = path_search(ctx, path, NULL);
     if (ref)
-      n = cJSON_GetObjectItem(ctx->tree, ref->file->uuid);
+      n = ref->members;
   }
   return n;
 }
@@ -298,17 +299,22 @@ static char *gen_path(remfs_ctx *ctx, size_t idx) {
   return fp.a;
 }
 
-static void add_to_tree(remfs_ctx *ctx, size_t idx, cJSON *root) {
+static void add_member(remfs_ctx *ctx, size_t idx, cJSON *root) {
   remfs_file *file = &kv_A(ctx->fv, idx);
 
-  cJSON *p = cJSON_CreateString(file->uuid);
   if (file->parent[0] == '\0') {
+    cJSON *p = cJSON_CreateString(file->uuid);
     cJSON_AddItemToArray(root, p);
   } else {
-    cJSON *n = cJSON_GetObjectItem(ctx->tree, file->parent);
+    cJSON *n = NULL;
+    uuid_map_node *ref = uuid_search(ctx, file->parent);
+    if (!ref)
+      return;
+    n = ref->members;
+    cJSON *p = cJSON_CreateString(file->uuid);
     if (!n) {
       n = cJSON_CreateArray();
-      cJSON_AddItemToObject(ctx->tree, file->parent, n);
+      ref->members = n;
     }
     cJSON_AddItemToArray(n, p);
   }
@@ -327,10 +333,7 @@ static void *remfs_init(struct fuse_conn_info *conn) {
   RB_INIT(ctx->fwd_map);
   RB_INIT(ctx->rev_map);
 
-  ctx->tree = cJSON_CreateObject();
-
-  cJSON *root = cJSON_CreateArray();
-  cJSON_AddItemToObject(ctx->tree, "root", root);
+  ctx->members = cJSON_CreateArray();
   for (int i = 0; i < kv_size(ctx->fv); i++) {
     remfs_file *file = &kv_A(ctx->fv, i);
     if (file->deleted)
@@ -346,7 +349,6 @@ static void *remfs_init(struct fuse_conn_info *conn) {
     uuid_map_node *s = calloc(1, sizeof(uuid_map_node));
     s->file = file;
     RB_INSERT(uuid_fwd_map, ctx->fwd_map, s);
-    add_to_tree(ctx, i, root);
   }
 
   for (int i = 0; i < kv_size(ctx->fv); i++) {
@@ -356,6 +358,7 @@ static void *remfs_init(struct fuse_conn_info *conn) {
       continue;
     s->path = gen_path(ctx, i);
     RB_INSERT(uuid_rev_map, ctx->rev_map, s);
+    add_member(ctx, i, ctx->members);
   }
   kv_destroy(tmp);
   /*
@@ -371,6 +374,7 @@ static void destroy_maps(remfs_ctx *ctx) {
   for (n = RB_MIN(uuid_rev_map, ctx->rev_map); n != NULL; n = nxt) {
     nxt = RB_NEXT(uuid_rev_map, ctx->rev_map, n);
     RB_REMOVE(uuid_rev_map, ctx->rev_map, n);
+    cJSON_Delete(n->members);
     free(n->path);
     free(n);
   }
@@ -382,7 +386,7 @@ static void remfs_destroy(void *arg) {
   remfs_ctx *ctx = (remfs_ctx *)arg;
   if (ctx) {
     destroy_maps(ctx);
-    cJSON_Delete(ctx->tree);
+    cJSON_Delete(ctx->members);
     kv_destroy(ctx->fv);
     free(ctx->src_dir);
     free(ctx);
